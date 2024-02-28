34664f595782f63b4e6db761eedf563f7394a035
Abhishek <152879837+AbhishekAbhishek1@users.noreply.github.com>
2024-02-28 18:53:47 +0100

    Update config.py

--- HyBayesCal/config.py
+++ HyBayesCal/config.py
@@ -1,3 +1,8 @@
+"""
+Author - Abhishek
+This script import the necessary Global and basic libraries"
+"""
+# basic python libraries
 try:
     import logging
     import os
@@ -6,7 +11,8 @@ except ModuleNotFoundError as basic:
         'ModuleNotFoundError: Missing basic libraries (required: logging, '
         'os)')
     print(basic)
-
+    
+# global python libraries
 try:
     import numpy as np
     import pandas as pd
@@ -19,14 +25,16 @@ except ModuleNotFoundError as additional:
     print(additional)
 
 # Define file paths
-input_worbook_name = "/home/amintvm/modeling/HyBayesCal-pckg-master/use-case-xlsx/tm-user-input_parameters.xlsx"
-activateTM_path = "/home/amintvm/modeling/HyBayesCal-pckg-master/env-scripts/activateTM.sh"
+
+input_worbook_name = "/home/amintvm/modeling/HyBayesCal-pckg-master/use-case-xlsx/tm-user-input_parameters.xlsx" # Path of use-input .xsls
+activateTM_path = "/home/amintvm/modeling/HyBayesCal-pckg-master/env-scripts/activateTM.sh" # path of the .sh file to activate the Telemac
 results_filename_base = "r2dsteady"  # Choose this according to how it is written in the .cas base file. Do not add the extension.
-output_excel_file_name="simulation_outputs.xlsx" # Choose a name for the **.xlsx output file.
-log_directory = '/home/amintvm/modeling/HyBayesCal-pckg-master/simulationxxxx/auto-saved-results' #Choose a directory where you want to save the logfile.log. 
+output_excel_file_name="simulation_outputs.xlsx" # Choose a name for the .xlsx output file.
+log_directory = '/home/amintvm/modeling/HyBayesCal-pckg-master/simulationxxxx/auto-saved-results' #Choose a directory where you want to save the logfile.log.
 node_file_name='nodes_input.txt' # file to filter the required Nodes
 node_file_path='/home/amintvm/modeling/HyBayesCal-pckg-master/HyBayesCal/' # Location to place the .txt file
 excel_file_path='/home/amintvm/modeling/Simulation/HyBayesCal-pckg-master/simulationxxxx/auto-saved-results/' #Location to place the simulation_outputs.xlsx file
+
 # Absolute path of the current directory
 this_dir = os.path.abspath(".")
 logfile_path = os.path.join(log_directory, "logfile.log")
